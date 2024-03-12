8f91438b09a48d999e48e944af6ef8c502b9cafd
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-13 00:13:58 +0600

    refactor: use hook to ensure dry code

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -3,18 +3,29 @@ import { visit } from "../lib/test-helpers"
 import { screen, waitForElementToBeRemoved } from "@testing-library/react"
 import makeServer from "../server";
 
+
+let server
+
+beforeEach(() => {
+  server = makeServer("test")
+})
+
+afterEach(() => {
+  server.shutdown()
+})
+
 test("it shows a message when there are no reminders", async () => {
-	let server = makeServer("test")
-	makeServer("test")
+//	let server = makeServer("test")
+//	makeServer("test")
 	visit("/")
 	await waitForElementToBeRemoved(() => screen.getByText("Loading..."))
 
 	expect(screen.getByText("All done!")).toBeInTheDocument()
-	server.shutdown()
+//	server.shutdown()
 })
 
 test("it shows existing reminders", async () => {
-	let server = makeServer("test")
+//	let server = makeServer("test")
 	server.create("reminder", { text: "Walk the dog" })
 	server.create("reminder", { text: "Take out the trash" })
 	server.create("reminder", { text: "Work out" })
@@ -28,5 +39,5 @@ test("it shows existing reminders", async () => {
 	expect(screen.getByText("Work out")).toBeInTheDocument()
 
 	//expect(screen.getByText("All done!")).toBeInTheDocument()
-	server.shutdown()
+//	server.shutdown()
   })
