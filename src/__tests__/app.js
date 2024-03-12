5f42edca94d842e18080d437e46d33eb0cce4c43
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-12 23:49:03 +0600

    feat: add test

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -1,1 +1,10 @@
 // Welcome to the test file for the tutorial!
+import { visit } from "../lib/test-helpers"
+import { screen, waitForElementToBeRemoved } from "@testing-library/react"
+
+test("it shows a message when there are no reminders", async () => {
+	visit("/")
+	await waitForElementToBeRemoved(() => screen.getByText("Loading..."))
+
+	expect(screen.getByText("All done!")).toBeInTheDocument()
+})
