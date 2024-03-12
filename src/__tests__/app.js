f4b2a80b6e3e1e367779ddd438ffe4aee3c3e472
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-13 00:07:54 +0600

    chore: add separate servers for separate tests

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -1,10 +1,24 @@
 // Welcome to the test file for the tutorial!
 import { visit } from "../lib/test-helpers"
 import { screen, waitForElementToBeRemoved } from "@testing-library/react"
+import makeServer from "../server";
 
 test("it shows a message when there are no reminders", async () => {
+	let server = makeServer("test")
+	makeServer("test")
 	visit("/")
 	await waitForElementToBeRemoved(() => screen.getByText("Loading..."))
 
 	expect(screen.getByText("All done!")).toBeInTheDocument()
+	server.shutdown()
 })
+
+test("it shows existing reminders", async () => {
+	let server = makeServer("test")
+	makeServer("test")
+	visit("/")
+	await waitForElementToBeRemoved(() => screen.getByText("Loading..."))
+  
+	expect(screen.getByText("All done!")).toBeInTheDocument()
+	server.shutdown()
+  })
